217ab4af58cbcdaf254e30ccaac377b9cfabe44f
krishnan <krishnan1994@gmail.com>
2024-06-23 02:26:43 -0400

    added claude 3.5

--- src/main.ts
+++ src/main.ts
@@ -205,6 +205,7 @@ class AiAssistantSettingTab extends PluginSettingTab {
 						"claude-3-opus-20240229": "Claude 3 Opus",
 						"claude-3-sonnet-20240229": "Claude 3 Sonnet",
 						"claude-3-haiku-20240307": "Claude 3 Haiku",
+						"claude-3-5-sonnet-20240620": "Claude 3.5 Sonnet",
 					})
 					.setValue(this.plugin.settings.modelName)
 					.onChange(async (value) => {
