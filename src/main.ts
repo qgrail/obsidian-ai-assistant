bb5e30be39611768d01fe8ab45511e8efb1fd1fc
qgrail <quentingrailpro@gmail.com>
2024-07-12 18:59:51 +0200

    fix: gpt-4o name

--- src/main.ts
+++ src/main.ts
@@ -199,7 +199,7 @@ class AiAssistantSettingTab extends PluginSettingTab {
 			.addDropdown((dropdown) =>
 				dropdown
 					.addOptions({
-						"gpt-4o	": "gpt-4o",
+						"gpt-4o": "gpt-4o",
 						"gpt-4": "gpt-4",
 						"gpt-4-turbo": "gpt-4-turbo",
 						"gpt-3.5-turbo": "gpt-3.5-turbo",
